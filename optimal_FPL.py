ff2d66a63c3eb388f6d5baca11d67fb1bc9eddc3
Colin Bowen <colinpbowen@gmail.com>
2020-11-30 22:53:01 -0500

    optimal team update

--- optimal_FPL.py
+++ optimal_FPL.py
@@ -6,39 +6,46 @@ import scipy
 
 #data_frame = pd.read_csv("FPLPlayerData.txt",
 # sep = "\t")
-data_frame = pd.read_csv("FPLPlayerData2.csv")
+#data_frame = pd.read_csv("FPLPlayerData2.csv")
+data_frame = pd.read_csv("~/fantasy-RL/player_initial_values_1819_FINAL.csv")
 #minutes = data_frame['minutes']
 #data_frame = data_frame[minutes > 900]
 num_players = len(data_frame)
 print("num_players = ", num_players)
 #print(data_frame.head())
 
-cost = data_frame['now_cost']
-player = data_frame['web_name']
-utilization = data_frame['selected_by_percent']
+###### 18/19 Data Read in ####################
+cost = data_frame['cost']
+player = data_frame['full_names'] 
+selected_by = data_frame['selected']
 team = data_frame['team']
-club = data_frame['club']
-position = data_frame['position']
-value = data_frame['value_season']
-points = data_frame['total_points']
-minutes = data_frame['minutes']
-ict = data_frame['ict_index_rank']
-cop = data_frame['chance_of_playing_next_round']
-elite_selected = data_frame['elite_selected']
-elite_selected = elite_selected.fillna(0)
+#player = data_frame['web_name']
 
-ppm = points/minutes
+#utilization = data_frame['selected_by_percent']
+#team = data_frame['team']
+#club = data_frame['club']
+#position = data_frame['position']
+#value = data_frame['value_season']
+#points = data_frame['total_points']
+#minutes = data_frame['minutes']
+#ict = data_frame['ict_index_rank']
+#cop = data_frame['chance_of_playing_next_round']
+#elite_selected = data_frame['elite_selected']
+#elite_selected = elite_selected.fillna(0)
 
-ppm_played = ppm[minutes > 90]
+#ppm = points/minutes
+
+#ppm_played = ppm[minutes > 90]
 #num_players = len(ppm_played)
 #vectorize these from Series
-utilization = utilization.values
+#utilization = utilization.values
+selected_by = selected_by.values
 cost = cost.values
-value = value.values
-points = points.values
-ppm = ppm.values
-ict = ict.values
-elite_selected = elite_selected.values
+#value = value.values
+#points = points.values
+#ppm = ppm.values
+#ict = ict.values
+#elite_selected = elite_selected.values
 #create numpy array for position matrix
 #positions = ['GK', 'D', 'M', 'F']
 GK_vec = data_frame.element_type == 1
@@ -88,8 +95,9 @@ use = m.addMVar(num_players, vtype=GRB.BINARY, name = "use")
 # here we maximize the inner product of utilization and use
 # can pick any quantity to maximize (ICT score, points last season) 
 
+m.setObjective(selected_by @ use, GRB.MAXIMIZE)
 #m.setObjective(utilization @ use, GRB.MAXIMIZE)
-m.setObjective(elite_selected @ use, GRB.MAXIMIZE)
+#m.setObjective(elite_selected @ use, GRB.MAXIMIZE)
 #m.setObjective(value @ use, GRB.MAXIMIZE)
 #m.setObjective(points @ use, GRB.MAXIMIZE)
 #m.setObjective(ppm_played @ use, GRB.MAXIMIZE)
@@ -104,18 +112,25 @@ m.addMConstrs(A=pos_matrix, x=use, sense='=',
 	b=pos_constrs)
 
 #add cost constraint
-m.addConstr(cost @ use <= 1002)
+m.addConstr(cost @ use <= 1000)
 
 m.optimize()
 
 var = m.getVars()
 
+variables = []
+
 for i in range(len(var)):
 	
+	variables.append(var[i].x)
 	if abs(var[i].x - 1) <= 1e-10:
-		print(player[i], club[i], cost[i]/10), position[i]# points[i], utilization[i], minutes[i], cop[i])
+		print(player[i], team[i], cost[i]/10)#, position[i]# points[i], utilization[i], minutes[i], cop[i])
 
 #for v in m.getVars():
 #	print(v.VarName, v.x)
 
 print('Obj:', m.Objval)
+
+var_series = pd.Series(variables)
+var_series.to_csv('~/fantasy-RL/optimal_starting_team_1819.csv')
+
